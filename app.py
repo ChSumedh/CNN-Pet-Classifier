0f042d3de388f0ce5a78aab286e9e65965ae7200
Sumedh <cherukurinagasumedh@gmail.com>
2026-08-09 19:22:04 +0530

    added postgresql support for persistent storage of the database

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ from train import fine_tune_model
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db_url = os.environ.get('DATABASE_URL', 'sqlite:///users.db')
+if db_url.startswith('postgres://'):
+    db_url = db_url.replace('postgres://', 'postgresql://', 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.secret_key = os.environ.get('SECRET_KEY', 'temporary-key')
