685ac4b8afe45ec09ef46ffeb175ac566fb4dbaf
ChSumedh <cherukurinagasumedh@gmail.com>
2026-08-10 20:38:54 +0530

    More changes to train.py

--- train.py
+++ train.py
@@ -1,29 +1,61 @@
 import io
 import gc
-import os
 
 import numpy as np
 import tensorflow as tf
 from PIL import Image
 
+
 CLASSES = [
-    'Abyssinian', 'Bengal', 'Birman', 'Bombay', 'British_Shorthair',
-    'Egyptian_Mau', 'Maine_Coon', 'Persian', 'Ragdoll', 'Russian_Blue',
-    'Siamese', 'Sphynx', 'american_bulldog', 'american_pit_bull_terrier',
-    'basset_hound', 'beagle', 'boxer', 'chihuahua', 'english_cocker_spaniel',
-    'english_setter', 'german_shorthaired', 'great_pyrenees', 'havanese',
-    'japanese_chin', 'keeshond', 'leonberger', 'miniature_pinscher',
-    'newfoundland', 'pomeranian', 'pug', 'saint_bernard', 'samoyed',
-    'scottish_terrier', 'shiba_inu', 'staffordshire_bull_terrier',
-    'wheaten_terrier', 'yorkshire_terrier'
+    'Abyssinian',
+    'Bengal',
+    'Birman',
+    'Bombay',
+    'British_Shorthair',
+    'Egyptian_Mau',
+    'Maine_Coon',
+    'Persian',
+    'Ragdoll',
+    'Russian_Blue',
+    'Siamese',
+    'Sphynx',
+    'american_bulldog',
+    'american_pit_bull_terrier',
+    'basset_hound',
+    'beagle',
+    'boxer',
+    'chihuahua',
+    'english_cocker_spaniel',
+    'english_setter',
+    'german_shorthaired',
+    'great_pyrenees',
+    'havanese',
+    'japanese_chin',
+    'keeshond',
+    'leonberger',
+    'miniature_pinscher',
+    'newfoundland',
+    'pomeranian',
+    'pug',
+    'saint_bernard',
+    'samoyed',
+    'scottish_terrier',
+    'shiba_inu',
+    'staffordshire_bull_terrier',
+    'wheaten_terrier',
+    'yorkshire_terrier'
 ]
 
-CLASS_MAP = {name: i for i, name in enumerate(CLASSES)}
+
+CLASS_MAP = {
+    name: index
+    for index, name in enumerate(CLASSES)
+}
 
 IMG_SIZE = (224, 224)
 
 
-def fine_tune_model(keras_model_path, feedbacks, epochs=1):
+def fine_tune_model(keras_model_path, feedbacks):
 
     model = None
 
@@ -43,27 +75,32 @@ def fine_tune_model(keras_model_path, feedbacks, epochs=1):
 
         print("Model loaded")
 
-        # Freeze everything
         for layer in model.layers:
             layer.trainable = False
 
-        # Only train the final layer
         model.layers[-1].trainable = True
 
-        model.compile(
-            optimizer=tf.keras.optimizers.Adam(
-                learning_rate=1e-5
-            ),
-            loss='sparse_categorical_crossentropy',
-            metrics=['accuracy']
+        optimizer = tf.keras.optimizers.Adam(
+            learning_rate=1e-5
         )
+
+        loss_fn = tf.keras.losses.SparseCategoricalCrossentropy()
+
+        trained = 0
 
         for fb in feedbacks:
 
             if fb.correct_class not in CLASS_MAP:
+                print(
+                    "Skipping unknown class:",
+                    fb.correct_class
+                )
                 continue
 
-            print("Processing feedback:", fb.id)
+            print(
+                "Processing feedback:",
+                fb.id
+            )
 
             img = Image.open(
                 io.BytesIO(fb.image)
@@ -76,40 +113,83 @@ def fine_tune_model(keras_model_path, feedbacks, epochs=1):
                 dtype=np.float32
             ) / 255.0
 
-            x = np.expand_dims(x, axis=0)
+            x = np.expand_dims(
+                x,
+                axis=0
+            )
 
-            y = np.array(
+            y = tf.constant(
                 [CLASS_MAP[fb.correct_class]],
-                dtype=np.int32
+                dtype=tf.int32
+            )
+
+            x = tf.convert_to_tensor(
+                x,
+                dtype=tf.float32
             )
 
             print("Training on one image...")
 
-            model.fit(
-                x,
-                y,
-                epochs=epochs,
-                batch_size=1,
-                verbose=1
+            with tf.GradientTape() as tape:
+
+                predictions = model(
+                    x,
+                    training=True
+                )
+
+                loss = loss_fn(
+                    y,
+                    predictions
+                )
+
+            gradients = tape.gradient(
+                loss,
+                model.trainable_variables
             )
+
+            optimizer.apply_gradients(
+                zip(
+                    gradients,
+                    model.trainable_variables
+                )
+            )
+
+            print(
+                "Feedback:",
+                fb.id,
+                "Loss:",
+                float(loss.numpy())
+            )
+
+            trained += 1
 
             del x
             del y
             del img
+            del predictions
+            del loss
+            del gradients
 
             gc.collect()
 
-        print("Saving Keras model...")
+        if trained == 0:
+            return (
+                False,
+                None,
+                "No usable feedback found"
+            )
+
+        print("Training finished")
 
         new_keras_path = "new_model.keras"
+
+        print("Saving Keras model...")
 
         model.save(
             new_keras_path
         )
 
         print("Converting to TFLite...")
-
-        gc.collect()
 
         converter = tf.lite.TFLiteConverter.from_keras_model(
             model
@@ -125,18 +205,34 @@ def fine_tune_model(keras_model_path, feedbacks, epochs=1):
         ) as f:
             f.write(tflite_model)
 
-        print("Fine tuning complete")
+        print("TFLite model saved")
 
-        return True, (
-            new_keras_path,
-            new_tflite_path
-        ), None
+        del tflite_model
+        del converter
+
+        gc.collect()
+
+        return (
+            True,
+            (
+                new_keras_path,
+                new_tflite_path
+            ),
+            None
+        )
 
     except Exception as e:
 
-        print("Fine tuning error:", e)
+        print(
+            "Fine-tuning error:",
+            repr(e)
+        )
 
-        return False, None, str(e)
+        return (
+            False,
+            None,
+            str(e)
+        )
 
     finally:
 
@@ -144,4 +240,5 @@ def fine_tune_model(keras_model_path, feedbacks, epochs=1):
             del model
 
         tf.keras.backend.clear_session()
+
         gc.collect()
